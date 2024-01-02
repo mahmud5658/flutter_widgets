472f23e59947f2040a6b82e5763f8c6ddbeb5a78
mahmud5658 <almahmuda5@gmail.com>
2024-01-02 16:42:53 +0600

    Safe area widget

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -19,72 +19,74 @@ class _AppBarWidgetState extends State<AppBarWidget> {
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 3,
-        child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.blue,
-              title: const Text(
-                'AppBar WidgetExample',
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ),
-              // centerTitle: true,
-              elevation: 30,
-              shadowColor: Colors.amberAccent,
-              toolbarOpacity: 1.0,
-              // toolbarHeight: 70,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              leading: const Icon(
-                Icons.menu,
-                color: Colors.white,
-              ),
-              actions: [
-                const Icon(
-                  Icons.favorite,
+        child: SafeArea(
+          child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.blue,
+                title: const Text(
+                  'AppBar WidgetExample',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                // centerTitle: true,
+                elevation: 30,
+                shadowColor: Colors.amberAccent,
+                toolbarOpacity: 1.0,
+                // toolbarHeight: 70,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                leading: const Icon(
+                  Icons.menu,
                   color: Colors.white,
                 ),
-                IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.search, color: Colors.white)),
-                const Icon(Icons.more_vert, color: Colors.white)
-              ],
-              bottom: const TabBar(
-                tabs: [
-                  Tab(
-                    icon: Icon(
-                      Icons.person,
-                      color: Colors.white,
-                    ),
-                    text: 'Person',
+                actions: [
+                  const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
                   ),
-                  Tab(
-                    icon: Icon(
-                      Icons.card_travel,
-                      color: Colors.white,
-                    ),
-                    text: 'Travel',
-                  ),
-                  Tab(
-                    icon: Icon(
-                      Icons.shopping_cart,
-                      color: Colors.white,
-                    ),
-                    text: 'Cart',
-                  )
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.search, color: Colors.white)),
+                  const Icon(Icons.more_vert, color: Colors.white)
                 ],
+                bottom: const TabBar(
+                  tabs: [
+                    Tab(
+                      icon: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                      ),
+                      text: 'Person',
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.card_travel,
+                        color: Colors.white,
+                      ),
+                      text: 'Travel',
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.shopping_cart,
+                        color: Colors.white,
+                      ),
+                      text: 'Cart',
+                    )
+                  ],
+                ),
               ),
-            ),
-            body: const TabBarView(
-              children: [
-                Person(),
-                Center(
-                  child: Text('This Travel Page'),
-                ),
-                Center(
-                  child: Text('This Cart Page'),
-                ),
-              ],
-            )),
+              body: const TabBarView(
+                children: [
+                  Person(),
+                  Center(
+                    child: Text('This Travel Page'),
+                  ),
+                  Center(
+                    child: Text('This Cart Page'),
+                  ),
+                ],
+              )),
+        ),
       ),
     );
   }
