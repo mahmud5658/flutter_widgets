0aa56f13014caee46a9a532ae19e847e4fb96cfc
mahmud5658 <almahmuda5@gmail.com>
2024-01-04 17:43:15 +0600

    flutter_widgets

--- lib/widgets/home_activity.dart
+++ lib/widgets/home_activity.dart
@@ -7,205 +7,205 @@ class HomeActivity extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          // centerTitle: true,
-          titleSpacing: 10,
-          toolbarHeight: 70,
-          // toolbarOpacity: 0.9,
-          elevation: 50,
-          title: const Text(
-            'Flutter',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.amber,
-          actions: [
-            IconButton(
-              onPressed: () {
-                snackBar("I am comment", context);
-              },
-              icon: const Icon(
-                Icons.comment,
-                color: Colors.white,
-              ),
+          appBar: AppBar(
+            // centerTitle: true,
+            titleSpacing: 10,
+            toolbarHeight: 70,
+            // toolbarOpacity: 0.9,
+            elevation: 50,
+            title: const Text(
+              'Flutter',
+              style: TextStyle(color: Colors.white),
             ),
-            IconButton(
-              onPressed: () {
-                snackBar("I am search", context);
-              },
-              icon: const Icon(
-                Icons.search,
-                color: Colors.white,
-              ),
-            ),
-            IconButton(
-              onPressed: () {
-                snackBar("I am  settings", context);
-              },
-              icon: const Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-            ),
-          ],
-          // leading: IconButton(
-          //   onPressed: () {
-          //     snackBar('I am menu button', context);
-          //   },
-          //   icon: const Icon(
-          //     Icons.menu,
-          //     color: Colors.white,
-          //   ),
-          // ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            snackBar('I am floating actions button', context);
-          },
-          elevation: 10,
-          backgroundColor: Colors.amber,
-          foregroundColor: Colors.white,
-          child: const Icon(Icons.add),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
+            backgroundColor: Colors.amber,
+            actions: [
+              IconButton(
+                onPressed: () {
+                  snackBar("I am comment", context);
+                },
+                icon: const Icon(
+                  Icons.comment,
                   color: Colors.white,
                 ),
-                label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.message,
+              ),
+              IconButton(
+                onPressed: () {
+                  snackBar("I am search", context);
+                },
+                icon: const Icon(
+                  Icons.search,
                   color: Colors.white,
                 ),
-                label: 'Message'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person,
+              ),
+              IconButton(
+                onPressed: () {
+                  snackBar("I am  settings", context);
+                },
+                icon: const Icon(
+                  Icons.settings,
                   color: Colors.white,
                 ),
-                label: 'Person'),
-          ],
-          onTap: (int index) {
-            if (index == 0) {
-              snackBar('I am home bottom menu', context);
-            } else if (index == 1) {
-              snackBar('I am contact bottom menu', context);
-            } else {
-              snackBar('I am profile bottom menu', context);
-            }
-          },
-          backgroundColor: Colors.amber,
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                padding: const EdgeInsets.all(0),
-                child: UserAccountsDrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Colors.amber,
-                  ),
-                  accountName: const Text('Abdullah Al Mahmud'),
-                  accountEmail: const Text('Info@abdullah.com'),
-                  currentAccountPicture: Image.network(
-                      'https://icons8.com/icon/7I3BjCqe9rjG/flutter'),
-                ),
-              ),
-              ListTile(
-                onTap: () {
-                  snackBar('Home', context);
-                },
-                leading: const Icon(Icons.home),
-                title: const Text('Home'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.contact_mail),
-                title: const Text('Contact'),
-                onTap: () {
-                  snackBar('Contact', context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text('Profile'),
-                onTap: () {
-                  snackBar('Profile', context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.email),
-                title: const Text('Email'),
-                onTap: () {
-                  snackBar('Email', context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.phone),
-                title: const Text('Phone'),
-                onTap: () {
-                  snackBar('Phone', context);
-                },
               ),
             ],
+            // leading: IconButton(
+            //   onPressed: () {
+            //     snackBar('I am menu button', context);
+            //   },
+            //   icon: const Icon(
+            //     Icons.menu,
+            //     color: Colors.white,
+            //   ),
+            // ),
           ),
-        ),
-        endDrawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                padding: const EdgeInsets.all(0),
-                child: UserAccountsDrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: Colors.amber,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              snackBar('I am floating actions button', context);
+            },
+            elevation: 10,
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.white,
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 0,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: Colors.white,
                   ),
-                  accountName: const Text('Abdullah Al Mahmud'),
-                  accountEmail: const Text('Info@abdullah.com'),
-                  currentAccountPicture: Image.network(
-                      'https://icons8.com/icon/7I3BjCqe9rjG/flutter'),
-                ),
-              ),
-              ListTile(
-                onTap: () {
-                  snackBar('Home', context);
-                },
-                leading: const Icon(Icons.home),
-                title: const Text('Home'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.contact_mail),
-                title: const Text('Contact'),
-                onTap: () {
-                  snackBar('Contact', context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text('Profile'),
-                onTap: () {
-                  snackBar('Profile', context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.email),
-                title: const Text('Email'),
-                onTap: () {
-                  snackBar('Email', context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.phone),
-                title: const Text('Phone'),
-                onTap: () {
-                  snackBar('Phone', context);
-                },
-              ),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.message,
+                    color: Colors.white,
+                  ),
+                  label: 'Message'),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  label: 'Person'),
             ],
+            onTap: (int index) {
+              if (index == 0) {
+                snackBar('I am home bottom menu', context);
+              } else if (index == 1) {
+                snackBar('I am contact bottom menu', context);
+              } else {
+                snackBar('I am profile bottom menu', context);
+              }
+            },
+            backgroundColor: Colors.amber,
           ),
-        ),
-      ),
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  padding: const EdgeInsets.all(0),
+                  child: UserAccountsDrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Colors.amber,
+                    ),
+                    accountName: const Text('Abdullah Al Mahmud'),
+                    accountEmail: const Text('Info@abdullah.com'),
+                    currentAccountPicture: Image.network(
+                        'https://icons8.com/icon/7I3BjCqe9rjG/flutter'),
+                  ),
+                ),
+                ListTile(
+                  onTap: () {
+                    snackBar('Home', context);
+                  },
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.contact_mail),
+                  title: const Text('Contact'),
+                  onTap: () {
+                    snackBar('Contact', context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text('Profile'),
+                  onTap: () {
+                    snackBar('Profile', context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.email),
+                  title: const Text('Email'),
+                  onTap: () {
+                    snackBar('Email', context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.phone),
+                  title: const Text('Phone'),
+                  onTap: () {
+                    snackBar('Phone', context);
+                  },
+                ),
+              ],
+            ),
+          ),
+          endDrawer: Drawer(
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  padding: const EdgeInsets.all(0),
+                  child: UserAccountsDrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Colors.amber,
+                    ),
+                    accountName: const Text('Abdullah Al Mahmud'),
+                    accountEmail: const Text('Info@abdullah.com'),
+                    currentAccountPicture: Image.network(
+                        'https://icons8.com/icon/7I3BjCqe9rjG/flutter'),
+                  ),
+                ),
+                ListTile(
+                  onTap: () {
+                    snackBar('Home', context);
+                  },
+                  leading: const Icon(Icons.home),
+                  title: const Text('Home'),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.contact_mail),
+                  title: const Text('Contact'),
+                  onTap: () {
+                    snackBar('Contact', context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.person),
+                  title: const Text('Profile'),
+                  onTap: () {
+                    snackBar('Profile', context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.email),
+                  title: const Text('Email'),
+                  onTap: () {
+                    snackBar('Email', context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.phone),
+                  title: const Text('Phone'),
+                  onTap: () {
+                    snackBar('Phone', context);
+                  },
+                ),
+              ],
+            ),
+          ),
+          body: Center(child: const Text('Hello World!'))),
     );
   }
 }
