e0b0fad2eebade2a40371b7cda231416d8f4cc6e
mahmud5658 <almahmuda5@gmail.com>
2024-01-05 19:09:38 +0600

    flutter widget

--- lib/widgets/listview1.dart
+++ lib/widgets/listview1.dart
@@ -14,18 +14,30 @@ class ListViewBuilder extends StatelessWidget {
         title: const Text('List View Builder',
             style: TextStyle(color: Colors.white)),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
-            
-          );
-        },
-      ),
+      // body: ListView.builder(
+      //   padding: const EdgeInsets.all(8),
+      //   itemCount: entries.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return Container(
+      //       height: 50,
+      //       color: Colors.amber[colorCodes[index]],
+      //       child: Center(child: Text('Entry ${entries[index]}')),
+      //     );
+      //   },
+      // ),
+
+      body: ListView.separated(
+        padding: const EdgeInsets.all(10),
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              color: Colors.amber[colorCodes[index]],
+              child: Center(child: Text('Entry ${entries[index]}')),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+          itemCount: entries.length),
     );
   }
 }
